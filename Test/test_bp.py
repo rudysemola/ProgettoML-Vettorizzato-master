64f734ffa4561aa780dfb40dde9702db5f248ad5
Michele <mikelefonty@gmail.com>
2019-02-05 16:05:02 +0100

    Modificato A0 per cm

---
+++ Test/test_bp.py
@@ -0,0 +1,78 @@
+import sys
+
+sys.path.append("../")
+from matplotlib import pyplot as plt
+from Trainers.TrainBackprop_new import *
+from MLP.Activation_Functions import *
+from MLP.MLP import *
+
+if __name__ == '__main__':
+    n_features = 10
+    n_hidden = 33
+    n_out = 2
+    eta = 0.01
+    alpha = 0.5
+    lambd = 0.0075
+    # lambd = 0
+    classification = False
+
+    P = loadMatrixFromFile("../Datasets/DatasetTrVl.csv")
+    X = P[:, : - 2]
+    T = P[:, -2:]
+
+    trainer = TrainBackprop2()
+    mlp = MLP(n_features, n_hidden, n_out, TanhActivation(), LinearActivation(), lambd=lambd, eta=eta, alfa=alpha,
+              trainer=trainer, classification=classification)
+
+    # train(self, mlp, X, T, X_val, T_val, n_epochs=1000, eps=1e-6, threshold=0.5, suppress_print=False):
+    mlp.trainer.train(mlp, addBias(X), T, addBias(X), T, n_epochs=10000, eps=1e-4, suppress_print=True)
+
+    plt.subplot(2, 1, 1)
+    plt.plot(mlp.errors_tr)
+    plt.grid(True)
+    plt.ylim([mlp.errors_tr[-1] - 0.5, mlp.errors_tr[-1] + 1])
+    plt.xlabel("Epochs")
+    plt.ylabel("MSE")
+
+    plt.subplot(2, 1, 2)
+    plt.plot(mlp.errors_mee_tr)
+    plt.grid(True)
+    plt.ylim([mlp.errors_mee_tr[-1] - 0.5, mlp.errors_mee_tr[-1] + 1])
+    plt.xlabel("Epochs")
+    plt.ylabel("MEE")
+    plt.show()
+
+"""
+    #PLOT DELLE LEARNING CURVE...
+    plt.plot(mlp.errors_tr)
+    plt.plot(mlp.errors_vl)
+    plt.grid(True)
+    plt.ylim((0,10))
+    plt.show()
+
+    plt.plot(mlp.errors_mee_tr)
+    plt.plot(mlp.errors_mee_vl)
+    plt.grid(True)
+    plt.ylim((0, 10))
+    plt.show()
+
+    #PLOT DEL GRADIENTE...
+    plt.loglog(mlp.gradients)
+    plt.grid(True)
+   # plt.ylim((0, 10))
+    plt.show()
+
+
+    print(mlp.errors_mee_tr)
+    #e = compute_obj_function(mlp,addBias(X),T,lambd)
+    #gradE_h,gradE_o = compute_gradient(mlp,addBias(X),T,lambd)
+
+    #phi,phi_p =f2phi(eta,mlp,addBias(X),T,gradE_h,gradE_o,lambd)
+    #print("Phi = %s, Phi primo = %s"%(phi,phi_p))
+    #mlp.trainer.train(mlp, addBias(X), Y, addBias(X_val), Y_val, 500, 1e-6)
+
+    #(mlp,X,T,phi_0,gradE_h,gradE_o,lambd,eta_start=1,eta_max=20,max_iter=100,m1=0.001,m2=0.9,tau = 0.9)
+    #eta_star = AWLS(mlp,addBias(X),T,e,gradE_h,gradE_o,lambd,eta_start=0.01,eta_max=4,max_iter=100)
+    print("Eta_star trovato: ",eta_star)
+"""
+
---
+++ Trainers/TrainBackprop_new.py
@@ -0,0 +1,169 @@
+import sys
+sys.path.append("../")
+
+from Trainers.Training import *
+from Utilities.UtilityCM2 import *
+
+""""
+Effettua lo standard SGD con momentum usando stepsize fissato.
+"""
+class TrainBackprop2(Training):
+
+    def __init__(self,path_results = "../RisultatiCM/bp.csv"):
+        self.w_prec = None
+        self.w_new = None
+
+        self.gradE = 0
+        self.gradE_0 = 0
+        self.epsilon_prime = 0
+        self.path_results = path_results
+        self.norm_gradE = 0
+        self.norm_gradE_0 = 0
+
+
+    def train(self,mlp,X, T, X_val, T_val, n_epochs = 1000, eps = 10 ^ (-3), threshold = 0.5, suppress_print = False):
+        assert X.shape[0] == T.shape[0]
+
+
+        epoch = 0
+        done_max_epochs = False  # Fatte numero massimo iterazioni
+        found_optimum = False  # Gradiente minore o uguale a eps_prime
+
+        while (not done_max_epochs) and (not found_optimum):
+            if epoch == 0:
+
+                self.w_prec = get_current_point(mlp)
+                self.w_new = self.w_prec
+                E, self.gradE = evaluate_function(mlp, X, T, self.w_new)
+
+                if mlp.classification:
+                    accuracy = compute_Accuracy_Class(T, convert2binary_class(mlp.Out_o, threshold))
+                    mlp.errors_tr.append(E)
+                    mlp.accuracies_tr.append(accuracy)
+                else:
+                    error_MEE = compute_Regr_MEE(T, mlp.Out_o)
+                    mlp.errors_tr.append(E)
+                    mlp.errors_mee_tr.append(error_MEE)
+
+                self.norm_gradE_0 = np.linalg.norm(self.gradE)
+
+                self.epsilon_prime = eps * self.norm_gradE_0
+                self.norm_gradE = self.norm_gradE_0
+
+                mlp.gradients.append(self.norm_gradE / self.norm_gradE_0)
+                with open(self.path_results, "w") as f:
+                    f.write("#Iterazione,Iterazioni spese in Line Search,Eta,Errore,Gradiente\n")
+
+            else:
+                self.w_new = get_current_point(mlp)
+                E, self.gradE = evaluate_function(mlp, X, T, self.w_new)
+
+                if mlp.classification:
+                    accuracy = compute_Accuracy_Class(T, convert2binary_class(mlp.Out_o, threshold))
+                    mlp.errors_tr.append(E)
+                    mlp.accuracies_tr.append(accuracy)
+                else:
+                    error_MEE = compute_Regr_MEE(T, mlp.Out_o)
+                    mlp.errors_tr.append(E)
+                    mlp.errors_mee_tr.append(error_MEE)
+
+                self.norm_gradE = np.linalg.norm(self.gradE)
+
+                mlp.gradients.append(self.norm_gradE / self.norm_gradE_0)
+
+            # CALCOLO IL VALIDATION ERROR
+            error_MSE_val, _ = evaluate_function(mlp, X_val, T_val, self.w_new)
+
+            if mlp.classification:
+                accuracy_val = compute_Accuracy_Class(T_val, convert2binary_class(mlp.Out_o, threshold))
+                mlp.errors_vl.append(error_MSE_val)
+                mlp.accuracies_vl.append(accuracy_val)
+            else:
+                error_MEE_val = compute_Regr_MEE(T_val, mlp.Out_o)
+                mlp.errors_vl.append(error_MSE_val)
+                mlp.errors_mee_vl.append(error_MEE_val)
+
+            # CONTROLLO GRADIENTE
+            if self.norm_gradE < self.epsilon_prime:
+                found_optimum = True
+
+            if not found_optimum:
+                # LINE_SEARCH
+                d = - self.gradE
+
+                # AGGIORNAMENTO
+
+                w_new = self.w_new + mlp.eta * d + (mlp.alfa * (self.w_new - self.w_prec))
+
+                self.w_prec = self.w_new
+                self.w_new = w_new
+
+                update_weights(mlp, self.w_new)
+
+                # per stampa per ogni epoca
+                if not suppress_print:
+                    if mlp.classification:
+                        print(
+                            "Epoch %s/%s) Eta = %s ||gradE||/ ||gradE_0|| = %s,TR Error(MSE) : %s VL Error(MSE) : %s TR Accuracy((N-num_err)/N) : %s VL Accuracy((N-num_err)/N) : %s" % (
+                                epoch + 1, n_epochs, mlp.eta, self.norm_gradE / self.norm_gradE_0, E,
+                                error_MSE_val, accuracy, accuracy_val))
+                    else:
+                        print(
+                            "Epoch %s/%s) Eta = %s ||gradE||/ ||gradE_0|| = %s\nTR Error(MSE) : %s VL Error(MSE) : %s TR (MEE) : %s VL ((MEE) : %s" % (
+                                epoch + 1, n_epochs, mlp.eta, self.norm_gradE / self.norm_gradE_0,
+                                E, error_MSE_val, error_MEE, error_MEE_val))
+
+                with open(self.path_results, "a") as f:
+                    f.write("%s,%s,%s,%s\n" % (epoch + 1, mlp.eta, E, self.norm_gradE / self.norm_gradE_0))
+
+                print("Iterazione %s) Eta = %s New Error %s New Gradient %s" % (
+                    epoch, mlp.eta, E, self.norm_gradE / self.norm_gradE_0))
+                epoch += 1
+                # CONTROLLO EPOCHE
+                if epoch >= n_epochs:
+                    done_max_epochs = True
+
+        # CALCOLO ERRORE DOPO L'ULTIMO AGGIORNAMENTO
+
+        E, _ = evaluate_function(mlp, X, T, self.w_new)
+
+        if mlp.classification:
+            accuracy = compute_Accuracy_Class(T, convert2binary_class(mlp.Out_o, threshold))
+            mlp.errors_tr.append(E)
+            mlp.accuracies_tr.append(accuracy)
+        else:
+            error_MEE = compute_Regr_MEE(T, mlp.Out_o)
+            mlp.errors_tr.append(E)
+            mlp.errors_mee_tr.append(error_MEE)
+
+            error_MSE_val, _ = evaluate_function(mlp, X_val, T_val, self.w_new)
+
+            if mlp.classification:
+                accuracy_val = compute_Accuracy_Class(T_val, convert2binary_class(mlp.Out_o, threshold))
+                mlp.errors_vl.append(error_MSE_val)
+                mlp.accuracies_vl.append(accuracy_val)
+
+            else:
+                error_MEE_val = compute_Regr_MEE(T_val, mlp.Out_o)
+                mlp.errors_vl.append(error_MSE_val)
+                mlp.errors_mee_vl.append(error_MEE_val)
+
+            # per stampa di risultato finale
+        if suppress_print:
+            if mlp.classification:
+                print(
+                        "Final Results: ||gradE||/ ||gradE_0|| = %s\nTR Error(MSE) : %s VL Error(MSE) : %s TR Accuracy((N-num_err)/N) : %s VL Accuracy((N-num_err)/N) : %s" % (
+                            self.norm_gradE / self.norm_gradE_0, mlp.errors_tr[-1], mlp.errors_vl[-1],
+                            mlp.accuracies_tr[-1], mlp.accuracies_vl[-1]))
+            else:
+                print(
+                        "Final Results:||gradE||/ ||gradE_0|| = %s\nTR Error(MSE) : %s VL Error(MSE) : %s TR (MEE) : %s VL (MEE) : %s" % (
+                            self.norm_gradE / self.norm_gradE_0, mlp.errors_tr[-1], mlp.errors_vl[-1],
+                            mlp.errors_mee_tr[-1], mlp.errors_mee_vl[-1]))
+
+        if found_optimum:
+            print("Trovato ottimo")
+        elif done_max_epochs:
+            print("Terminato il numero massimo di iterazioni disponibili")
+
+        return len(mlp.errors_tr)
